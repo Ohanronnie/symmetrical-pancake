ac37b5f531d4fd052061ff56f7b918bdfb50bf36
Ohanronnie <titiloyepaul68@gmail.com>
2023-10-21 12:25:35 +0100

    added some features

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,6 +1,6 @@
 import _axios from "axios";
 export const axios = _axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env["VITE_BACKEND_URL"],
   withCredentials: true,
   timeout: 1000 * 60 * 60 * 24,
 });
